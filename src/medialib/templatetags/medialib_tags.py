5c7b53cb68f05f79458c07efb0f68c6e0246680d
Andre Engelbrecht <andre@teh-node.co.za>
2010-03-26 16:54:01 +0000

    Added support for multiple ckeditor instances

--- src/medialib/templatetags/medialib_tags.py
+++ src/medialib/templatetags/medialib_tags.py
@@ -4,22 +4,20 @@ from django.conf import settings
 register = template.Library()
 
 @register.inclusion_tag('medialib/cke_mediabrowser_head.html')
-def cke_mediabrowser_head(field_id='id_content'):
+def cke_mediabrowser_head(fields='id_content'):
     """
     Renders the template which includes the HEAD section for the CKEditor media
     browser.
 
-    ``field_label`` is the label of the field that will use the media browser.
-    ``ajax_loader_location`` is the location for the gif loader. Defaults to the
-    MEDIA_URL setting.
+    ``fields`` is the label of the field that will use the media browser.
+
+    Multiple field id's can be passed to the tag by seperating them with spaces
+    for example:
+    {% cke_mediabrowser_head 'id_content id_other_content' %}
 
     """
     MEDIA_URL = getattr(settings, 'MEDIA_URL', '/media/')
     AJAX_LOADER_LOCATION = getattr(settings, 'AJAX_LOADER_LOCATION') 
     CKEDITOR_SCRIPT_LOCATION = getattr(settings, 'CKEDITOR_SCRIPT_LOCATION')
-
-    return {
-        'field_id': field_id,
-        'AJAX_LOADER_LOCATION': AJAX_LOADER_LOCATION,
-        'CKEDITOR_SCRIPT_LOCATION': CKEDITOR_SCRIPT_LOCATION,
-    }
+    field_ids = fields.split(' ')
+    return locals()
